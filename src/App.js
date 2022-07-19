a9d274a69ca773b66120c1191e4e6b1dbc651bbf
EdvinasLi <105659762+EdvinasLi@users.noreply.github.com>
2022-07-19 12:40:35 +0300

    Films

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+const App = () => {
+  const API_KEY = "584bc648";
+  const REQUEST_URL =
+    "https://www.omdbapi.com/?apikey=" + API_KEY + "&s=Batman&page=";
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [movies, setMovies] = useState([]);
+  const [total, setTotal] = useState(0);
 
-function App() {
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(REQUEST_URL + page)
+      .then((resp) => resp.json())
+      .then((resp) => {
+        setIsLoading(false);
+
+        setMovies(resp.Search);
+        setTotal(Math.ceil(resp.totalResults / 10));
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        console.log(err);
+        setMovies([]);
+        setTotal(0);
+      });
+  }, [page]);
+  const prev = () => {
+    return page > 1 && setPage(page - 1);
+  };
+  const next = () => {
+    return page < total && setPage(page + 1);
+  };
+  const Pagination = () => {
+    return (
+      <div className="pagination">
+        <button onClick={prev} disabled={page === 1 && "disabled"}>
+          Ankstesnis
+        </button>
+        <span> Esamas puslapis {page}</span>
+        <button onClick={next} disabled={page === total && "disabled"}>
+          Sekantis
+        </button>
+      </div>
+    );
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>Filmu sarasas</p>
+      <div className="grid">
+        {movies.map((movie) => (
+          <div key={movie.imdbID} className="movie">
+            <div className="image">
+              <img src={movie.Poster} alt={movie.Title} />
+            </div>
+            <div className="title">{movie.Title}</div>
+            <div className="year">{movie.Year} </div>
+            <div className="type">{movie.Type} </div>
+          </div>
+        ))}
+      </div>
+      <div>{isLoading ? "Kraunasi..." : <Pagination />}</div>
+    </>
   );
-}
+};
 
 export default App;
